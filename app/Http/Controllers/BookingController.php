725c9b821304ab62a4e32c1c3144f1fb685dd6a8
falaFulani <alexmashua@gmail.com>
2020-08-19 22:38:24 +0300

    seeding plus branch

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\DB;
+
+use App\Booking;
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $bookings = DB::table('bookings')->get();
+       return view('bookings.index')
+        ->with('bookings', $bookings);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $rooms= DB::table('rooms')->get()->pluck('number','id')->prepend('Select a room');
+        $users= DB::table('users')->get()->pluck('name','id')->prepend('select user');
+        return view('bookings.create')
+            ->with('rooms', $rooms)
+        ->with('users', $users);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        dd($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Booking $booking)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Booking  $booking
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Booking $booking)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/test', 'RoomsController');
+
+Route::get('/rooms','RoomsController');
+Route::resource('/bookings', 'BookingController');
